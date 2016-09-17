c54389814107d3560c77edb7eb419ec3c795c7f4
Alicia Sykes <sykes.alicia@gmail.com>
2016-09-17 22:07:17 +0100

    gulp now compliles client-side Coffee

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,8 @@ var gulp = require('gulp'),
   nodemon = require('gulp-nodemon'),
   plumber = require('gulp-plumber'),
   livereload = require('gulp-livereload'),
-  sass = require('gulp-sass');
+  sass = require('gulp-sass'),
+  coffee = require('gulp-coffee');
 
 gulp.task('sass', function () {
   gulp.src('./public/css/*.scss')
@@ -12,8 +13,17 @@ gulp.task('sass', function () {
     .pipe(livereload());
 });
 
+gulp.task('coffee', function (){
+  gulp.src('./public/js/*.coffee')
+      .pipe(plumber())
+      .pipe(coffee())
+      .pipe(gulp.dest('./public/js'))
+      .pipe(livereload())
+});
+
 gulp.task('watch', function() {
   gulp.watch('./public/css/*.scss', ['sass']);
+  gulp.watch('./public/js/*.coffee', ['coffee']);
 });
 
 gulp.task('develop', function () {
